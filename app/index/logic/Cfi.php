f1318de800bf04d9e9b21321c34f2f435580c589
yuhuih052 <yuhuih052@gmail.com>
2020-08-02 15:36:46 +0800

    购买股票

--- app/index/logic/Cfi.php
+++ app/index/logic/Cfi.php
@@ -27,7 +27,7 @@ class CFI extends IndexBase
 		if($pay > $member['dianzibi'] ){
 			return [RESULT_SUCCESS,'电子币余额不足'];
 		}
-		$this->modelMember->where('id',$id)->dec('dianzibi',$pay)->update();
+		//$this->modelMember->where('id',$id)->dec('dianzibi',$pay)->update();
 		$buyer = $this->modelShop->where('user_id',$id)->find();
 
 		if(empty($buyer)){
@@ -43,23 +43,124 @@ class CFI extends IndexBase
 				'dianzibi' => $buyer->dianzibi + $pay,
 				'update_time' => time(),
 			];
-			//$this->modelShop->where('user_id',$id)->update($re);
+			$this->modelShop->where('user_id',$id)->update($re);
 			//$this->bill($id,$member['username'],'dianzibi_all',$pay,'挂买CFI');
 		}
-		$this->transaction($id,1);
-		return [RESULT_SUCCESS,'挂买成功'];
+		$re = $this->transaction($id,1,$member_rank);
+		
+		switch($re){
+			case 1 ;
+			return [RESULT_SUCCESS,'交易成功'];break;
+			case 2 ;
+			return [RESULT_SUCCESS,'交易成功,当前价格已刷新，请留意交易价格,如继续购买请点击继续'];break;
+		}
+		
 
 	}
-
-	public function transaction($id,$status){
+	//购买逻辑
+	public function transaction($id,$status,$member_rank){
+		
 		$now_price = $this->price()->cfi_price;
+		$cfi_total = $this->price()->cfi_total;
 		$buyer_list = $this->modelShop->where('dianzibi','>',0)->select();
-		$seller_list = $this->modelShop->where('sell','>',0)->select();
-		//当没有用户挂卖时，从系统账户购买
-		if(count($seller_list) == 0){
+		$seller_list = $this->modelShop->where('sell','>',0)->select(); 
+		//当没有用户挂卖且系统账户有剩余，从系统账户购买
+		if(count($seller_list) == 0 && $cfi_total >0){
 			$buyer = $this->modelShop->where('user_id',$id)->find();
-			
-		}else{
+			//当前账户挂买需求能购买的个数
+			$amount = floor($buyer->dianzibi / $now_price);
+			//判断账户目前能购买的最大数
+			$b = $member_rank['CFI_split'] - $this->info()->CFI;
+			//距离涨价的个数
+			$rise_ca = $this->price()->default_deal - $this->price()->deal;
+			if($rise_ca >= $amount){
+				if($cfi_total >$amount){
+					if($b >= $amount){
+						$this->modelShop->where('user_id',$id)->update(['dianzibi'=>0]);
+						$this->modelMember->where('id',$id)->inc('CFI',$amount)->update();
+						$this->priceUd($now_price,$this->price()->deal+$amount,$this->price()->cfi_total - $amount);
+						return 1;
+					}
+					$pp = $b * $now_price;
+					$this->modelShop->where('user_id',$id)->dec('dianzibi',$pp)->update();
+					$this->modelMember->where('id',$id)->inc('CFI',$b)->update();
+					$this->priceUd($now_price,$this->price()->deal+$b,$this->price()->cfi_total - $b);
+					return 1;
+				}else{
+					if($b >= $cfi_total){
+						$this->modelShop->where('user_id',$id)->dec('daiznibi',$now_price * $cfi_total)->update();
+						$this->modelMember->where('id',$id)->inc('CFI',$cfi_total)->update();
+						$this->priceUd($now_price,$this->price()->deal+$cfi_total,$this->price()->cfi_total - $cfi_total);
+						return 1;
+					}
+					$pp = $b * $now_price;
+					$this->modelShop->where('user_id',$id)->dec('dianzibi',$pp)->update();
+					$this->modelMember->where('id',$id)->inc('CFI',$b)->update();
+					$this->priceUd($now_price,$this->price()->deal+$b,$this->price()->cfi_total-$b);
+					return 1;
+				}
+			}else{
+				$after_buy = $amount - $rise_ca;
+				//如果账户接近封顶数
+				if($b < $rise_ca){
+					$pp = $b * $now_price;
+					$this->modelShop->where('user_id',$id)->dec('dianzibi',$pp)->update();
+					$this->modelMember->where('id',$id)->inc('CFI',$b)->update();
+					$this->priceUd($now_price,$this->price()->deal+$b,$this->price()->cfi_total-$b);
+					return 1;
+				}
+				//先结算未涨价部分
+				$this->modelShop->where('user_id',$id)->dec('daiznibi',$now_price * $rise_ca)->update();
+				$this->modelMember->where('id',$id)->inc('CFI',$rise_ca)->update();
+				//刷新购买者账户信息
+				$buyer = $this->modelShop->where('user_id',$id)->find();
+				//
+				
+				//结算后，价格上涨0.1；
+				$this->priceUd($now_price +0.1,0,$this->price()->cfi_total - $rise_ca);
+				//判断涨价后是否达到拆分条件
+				if($this->price()->cfi_price >=4){
+					$this->splitCfi();
+					return 2;
+				}else{//没达到拆分条件
+					//账户当前能购买的数量
+					$after_amount = floor($buyer->dianzibi / $this->price()->cfi_price);
+					//距离涨价的个数
+					$rise_ca = $this->price()->default_deal - $this->price()->deal;
+					$now_price = $this->price()->cfi_price;
+					$cfi_total = $this->price()->cfi_total;
+					//再次交易
+					if($cfi_total >$after_amount){
+						//如果账户接近封顶数
+						if($b < $after_amount){
+							$pp = $b * $now_price;
+							$this->modelShop->where('user_id',$id)->dec('dianzibi',$pp)->update();
+							$this->modelMember->where('id',$id)->inc('CFI',$b)->update();
+							$this->priceUd($now_price,$this->price()->deal+$b,$this->price()->cfi_total-$b);
+							return 1;
+						}
+						$this->modelShop->where('user_id',$id)->update(['dianzibi'=>0]);
+						$this->modelMember->where('id',$id)->inc('CFI',$after_amount)->update();
+						return 2;
+					}else{
+						//如果账户接近封顶数
+						if($b < $cfi_total){
+							$pp = $b * $now_price;
+							$this->modelShop->where('user_id',$id)->dec('dianzibi',$pp)->update();
+							$this->modelMember->where('id',$id)->inc('CFI',$b)->update();
+							$this->priceUd($now_price,$this->price()->deal+$b,$this->price()->cfi_total-$b);
+							return 1;
+						}
+						$this->modelShop->where('user_id',$id)->dec('daiznibi',$now_price * $cfi_total)->update();
+						$this->modelMember->where('id',$id)->inc('CFI',$cfi_total)->update();
+						return 2;
+					}
+
+				}
+			}
+
+			//交易市场有人挂卖时
+		}elseif(!count($seller_list) == 0){
 			foreach ($seller_list as $key => $v) {
 			dd($v);
 			}
@@ -104,6 +205,7 @@ class CFI extends IndexBase
 		$pre = [
 				'cfi_price' =>2,
 				'deal' => 0,
+				'default_deal' => $this->price()->default_deal *$a,
 				'cfi_total' => $this->price()->cfi_total *$a,
 			];
 		$this->modelPrice->where('id',1)->update($pre);
---
+++ runtime/temp/c5d71ddc2e7d3bbb958b28e1e997bc0f.php
@@ -0,0 +1,105 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:5:{s:69:"/home/workplace/zs/obtest2/public/../app/index/view/cfi/cfi_shop.html";i:1596203213;s:63:"/home/workplace/zs/obtest2/public/../app/index/view/layout.html";i:1585716400;s:67:"/home/workplace/zs/obtest2/public/../app/index/view/layout/top.html";i:1585716400;s:70:"/home/workplace/zs/obtest2/public/../app/index/view/layout/header.html";i:1595917326;s:70:"/home/workplace/zs/obtest2/public/../app/index/view/layout/footer.html";i:1585716400;}*/ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>OneBase开源架构|PHP开源系统</title>
+    <meta name="keywords" content="OneBase,PHP开源系统,ThinkPHP5,TP5,PHP框架,PHP源码"/>
+    <meta name="description" content="一款基于ThinkPHP5研发的开源免费基础架构，基于OneBase可以快速的研发各类应用。" />
+    <link href="__STATIC__/module/common/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="__STATIC__/module/common/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="__STATIC__/module/index/css/docs.css" rel="stylesheet">
+    <link href="__STATIC__/module/index/css/onebase.css" rel="stylesheet">
+    
+    <script type="text/javascript" src="__STATIC__/module/common/jquery/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="__STATIC__/module/common/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="__STATIC__/module/index/js/common.js"></script>
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="brand" href="<?php echo url('index/home'); ?>">OneBase</a>
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                        <li>
+                            <a href="<?php echo url('index/home'); ?>">首页</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo url('index/index'); ?>">新闻公告</a>
+                        </li>
+
+                        <?php if(\think\Request::instance()->session('user_id2') != null): ?>
+                          <li><a href="<?php echo url('user/index',['id'=> \think\Request::instance()->session('user_id2')]); ?>">个人中心</a></li>
+                          
+                        <?php else: ?>
+                         <li>
+                        <a target="_blank" href="<?php echo url('login/login'); ?>">登录</a>
+                    </li>
+
+                    <li>
+                        <a target="_blank" href="<?php echo url('login/register'); ?>">注册</a>
+                    </li>
+                    <li>
+                            <a target="_blank" href="<?php echo URL_ROOT; ?>/admin.php">后台管理</a>
+                        </li>
+                        <?php endif; ?>
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <table  class="table table-bordered">
+      <thead>
+      <tr>
+          <th>当前cfi价格:<?php echo $list['cfi_price']; ?></th>
+          <th>买入</th>  
+          <th>卖出</th>
+      </tr>
+      </thead>
+        <tbody>
+                <tr>
+                  <td></td>
+                  <td>当前价格：$<?php echo $list['cfi_price']; ?></td>
+                  <td>当前价格：$<?php echo $list['cfi_price']; ?></td> 
+                </tr>
+                <tr>
+                  <td>操作</td>
+                  <form action="<?php echo url('Cfi/sys_buy'); ?>" method="post">
+                  <td><input type="number" name="cfi_amount" value="">
+                    <input hidden="" name="cfi_price" value="<?php echo $list['cfi_price']; ?>">
+                      <button type="submit">买入</button>
+                  </td>
+                  </form>
+                  <form action="" method="post">
+                  <td><input type="number" name="cfi_amount" value="">
+                    <input hidden="" name="cfi_price" value="<?php echo $list['cfi_price']; ?>">
+                      <button type="submit">卖出</button>
+                  </td>
+                  </form>
+                </tr>
+       
+        </tbody>
+    </table>
+ 
+<footer class="footer">
+  <div class="container">
+      <p> 本站由 <strong><a href="http://www.onebase.org" target="_blank">OneBase</a></strong> 强力驱动</p>
+  </div>
+</footer>
+
+<script type="text/javascript" src="__STATIC__/module/index/js/footer.js"></script>
+
+<?php echo hook('hook_view_index'); ?>
+
+</body>
+</html>
